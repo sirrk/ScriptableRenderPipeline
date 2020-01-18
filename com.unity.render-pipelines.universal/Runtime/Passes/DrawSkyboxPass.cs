5ba18a4cb91e8fbb308a2f0934cd6e012bf9778d
Thomas Zeng <thomas.zeng@unity3d.com>
2020-01-17 16:00:20 -0800

    * Added Pure XRSDK URP Skybox path workaround.   * TODO: Requires to move skybox path to URP land to be compatible with pure XRSDK solution.     Current skybox is using build-in shaders, bypassing URP shader includes

--- com.unity.render-pipelines.universal/Runtime/Passes/DrawSkyboxPass.cs
+++ com.unity.render-pipelines.universal/Runtime/Passes/DrawSkyboxPass.cs
@@ -15,7 +15,37 @@ namespace UnityEngine.Rendering.Universal.Internal
         /// <inheritdoc/>
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            context.DrawSkybox(renderingData.cameraData.camera);
+            // Setup Legacy XR buffer states
+            if (renderingData.cameraData.xrPass.hasMultiXrView)
+            {
+                // Setup legacy XR stereo buffer
+                Debug.Assert(renderingData.cameraData.xrPass.viewCount == 2, "View Count must be 2, other view count is not implemented yet!");
+                renderingData.cameraData.camera.SetStereoProjectionMatrix(Camera.StereoscopicEye.Left, renderingData.cameraData.xrPass.GetProjMatrix(0));
+                renderingData.cameraData.camera.SetStereoViewMatrix(Camera.StereoscopicEye.Left, renderingData.cameraData.xrPass.GetViewMatrix(0));
+                renderingData.cameraData.camera.SetStereoProjectionMatrix(Camera.StereoscopicEye.Right, renderingData.cameraData.xrPass.GetProjMatrix(1));
+                renderingData.cameraData.camera.SetStereoViewMatrix(Camera.StereoscopicEye.Right, renderingData.cameraData.xrPass.GetViewMatrix(1));
+
+                // Use legacy stereo instancing mode to have legacy XR code path configured
+                CommandBuffer cmd = CommandBufferPool.Get();
+                cmd.SetSinglePassStereo(SinglePassStereoMode.Instancing);
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+
+                // Calling into build in skybox pass
+                context.DrawSkybox(renderingData.cameraData.camera);
+
+                // Disable Legacy XR path
+                cmd.SetSinglePassStereo(SinglePassStereoMode.None);
+
+                // Reset legacy XR stereo buffer
+                renderingData.cameraData.camera.ResetStereoProjectionMatrices();
+                renderingData.cameraData.camera.ResetStereoViewMatrices();
+                CommandBufferPool.Release(cmd);
+            }
+            else
+            {
+                context.DrawSkybox(renderingData.cameraData.camera);
+            }
         }
     }
 }
